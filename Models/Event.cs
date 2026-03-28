30348c22d8ad5d51cd694ad3352c07288815edc7
LeThZ25 <duonglethuong2005@gmail.com>
2026-03-28 18:18:20 +0700

    Model for Event (Basic)

---
+++ Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace Campus_Activity_Manager.Models
+{
+	public class Event
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime Date { get; set; }
+		public string Location { get; set; }
+		public string Image { get; set; }
+		public string Category { get; set; }
+		public bool IsRegistered { get; set; }
+	}
+}
