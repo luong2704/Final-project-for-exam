8d28d7438b6a15fe27a148c1a32483eb42a9532a
Aeron <aeronbtw@gmail.com>
2026-03-28 19:39:13 +0700

    refactor(#39): simplify Category model - remove validation attributes

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,23 +1,12 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Campus.Models;
 
 public class Category
 {
     public int CategoryId { get; set; }
-
-    [Required]
-    [MaxLength(100)]
     public string CategoryName { get; set; } = string.Empty;
-
-    [MaxLength(500)]
     public string? CategoryDescription { get; set; }
-
     public CategoryType CategoryType { get; set; }
-
     public bool IsActive { get; set; } = true;
-
     public DateTime CreatedDate { get; set; } = DateTime.Now;
-
     public DateTime UpdatedDate { get; set; } = DateTime.Now;
 }
