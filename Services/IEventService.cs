ce8e9d068d036b2c2c07bb50c321995fb7968ebb
haluu07 <hatheluu1007@gmail.com>
2026-03-28 21:52:26 +0700

    feat: add RegisterEventAsync method to IEventService interface

--- Services/IEventService.cs
+++ Services/IEventService.cs
@@ -1,10 +1,11 @@
-﻿using Campus.Models;
+using Campus.Models;
 
 namespace Campus.Services
 {
 	public interface IEventService
 	{
 		Task<List<Event>> GetMyEventsAsync();
+		Task<bool> RegisterEventAsync(Guid eventId);
 		Task<bool> UnregisterEventAsync(Guid eventId);
 	}
 }
