623ee6fb229e1b80e66713cf54acd3c87efcb690
Minh <leeanhminhw@gmail.com>
2026-03-29 22:06:59 +0700

    Initialize LoginViewModel in LoginPage.xaml.cs

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -5,6 +5,9 @@ public partial class LoginPage : ContentPage
 	public LoginPage()
 	{
 		InitializeComponent();
+
+        // Set binding context to the view model
+        this.BindingContext = new Campus.ViewModels.LoginViewModel();
 	}
     private async void OnForgotPasswordTapped(object sender, EventArgs e)
     {
