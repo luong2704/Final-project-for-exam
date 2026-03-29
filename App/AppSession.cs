7bd70b64dde4eca85128032a09c3fb0343f5fd38
Minh <leeanhminhw@gmail.com>
2026-03-29 18:13:53 +0700

    Add AppSession to store current user

---
+++ App/AppSession.cs
@@ -0,0 +1,17 @@
+﻿
+using Campus.Models;
+
+namespace Campus.App
+{
+    public static class AppSession
+    {
+        public static User CurrentUser { get; set; }
+
+        public static bool IsLoggedIn => CurrentUser != null;
+
+        public static void Logout()
+        {
+            CurrentUser = null;
+        }
+    }
+}
