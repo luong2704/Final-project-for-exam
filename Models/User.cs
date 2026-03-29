533f81e15af0c9eac47fea41771c12ad78cff520
Minh <leeanhminhw@gmail.com>
2026-03-29 18:02:57 +0700

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campus.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
