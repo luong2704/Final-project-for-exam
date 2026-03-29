69230bfa6331b4b41fb9609f5322fd7641e33239
Minh <leeanhminhw@gmail.com>
2026-03-29 18:07:10 +0700

    Add UserService for login logic

---
+++ Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Campus.Models;
+
+namespace Campus.Services
+{
+    public class UserService
+    {
+        private List<User> _users = new List<User>
+        {
+            new User { Id = 1, Username = "admin", Password = "123", Email = "admin@gmail.com" },
+            new User { Id = 2, Username = "user", Password = "123", Email = "user@gmail.com" }
+        };
+
+        public User Login(string username, string password)
+        {
+            return _users.FirstOrDefault(u =>
+                u.Username == username && u.Password == password);
+        }
+    }
+}
