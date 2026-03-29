7890e2774a191f854b1e980dca6d3d356ce2ba03
cindy <admin@LAPTOP-4L802QDE>
2026-03-30 00:34:56 +0700

    Create SettingsViewModel skeleton

---
+++ ViewModels/SettingsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Campus.Services;
+
+namespace Campus.ViewModels
+{
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        private readonly SettingsService _settingsService;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public SettingsViewModel()
+        {
+            _settingsService = new SettingsService();
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
