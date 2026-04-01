24e20d3001182133e3adc95a49de817349588939
hhuong103 <253066440+hhuong103@users.noreply.github.com>
2026-04-01 13:48:01 +0700

    Set BindingContext for SettingsPage

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,3 +1,5 @@
+using Campus.ViewModels;
+
 namespace Campus.Views;
 
 public partial class SettingsPage : ContentPage
@@ -5,5 +7,6 @@ public partial class SettingsPage : ContentPage
 	public SettingsPage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new SettingsViewModel();
+    }
 }
