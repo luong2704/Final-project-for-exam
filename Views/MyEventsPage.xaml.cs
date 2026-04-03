c4affb58e9df94bfd3ae7ba2e77a1272843a97c3
Huyntrangxinh <lahuyentrang25678@gmail.com>
2026-04-03 23:14:06 +0700

    feat(T5-20): implement EmptyView state for CollectionView in MyEventsPage

--- Views/MyEventsPage.xaml.cs
+++ Views/MyEventsPage.xaml.cs
@@ -7,6 +7,7 @@ public partial class MyEventsPage : ContentPage
    private readonly EventViewModels _viewModel;
 
 
+
    public MyEventsPage(EventViewModels viewModel)
    {
        InitializeComponent();
