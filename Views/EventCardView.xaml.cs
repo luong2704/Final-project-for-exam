f84da859f240ac8424a406e2d77cb8aaf53cd028
chloe259 <trinhphuongthao259@gmail.com>
2026-04-08 16:59:14 +0700

    feat(task-8): update EventCardView add TapCommand and ShowActions - Thao

--- Views/EventCardView.xaml.cs
+++ Views/EventCardView.xaml.cs
@@ -4,18 +4,35 @@ namespace Campus.Views;
 
 public partial class EventCardView : ContentView
 {
-	public static readonly BindableProperty UnregisterCommandProperty =
-		BindableProperty.Create(nameof(UnregisterCommand), typeof(ICommand), typeof(EventCardView));
+    public static readonly BindableProperty UnregisterCommandProperty =
+        BindableProperty.Create(nameof(UnregisterCommand), typeof(ICommand), typeof(EventCardView));
 
+    public ICommand UnregisterCommand
+    {
+        get => (ICommand)GetValue(UnregisterCommandProperty);
+        set => SetValue(UnregisterCommandProperty, value);
+    }
 
-	public ICommand UnregisterCommand
-	{
-		get => (ICommand)GetValue(UnregisterCommandProperty);
-		set => SetValue(UnregisterCommandProperty, value);
-	}
+    public static readonly BindableProperty TapCommandProperty =
+        BindableProperty.Create(nameof(TapCommand), typeof(ICommand), typeof(EventCardView));
 
-	public EventCardView()
-	{
-		InitializeComponent();
-	}
+    public ICommand TapCommand
+    {
+        get => (ICommand)GetValue(TapCommandProperty);
+        set => SetValue(TapCommandProperty, value);
+    }
+
+    public static readonly BindableProperty ShowActionsProperty =
+        BindableProperty.Create(nameof(ShowActions), typeof(bool), typeof(EventCardView), true);
+
+    public bool ShowActions
+    {
+        get => (bool)GetValue(ShowActionsProperty);
+        set => SetValue(ShowActionsProperty, value);
+    }
+
+    public EventCardView()
+    {
+        InitializeComponent();
+    }
 }
